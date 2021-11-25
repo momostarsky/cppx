102fa7d18dfcb1ac055beb371f36310776e6d84e
momostarsky <33349170@qq.com>
2021-11-25 17:43:28 +0800

    DIcom Codec

---
+++ app/demox.cpp
@@ -0,0 +1,13 @@
+//
+// Created by dhz on 2021/11/25.
+//
+
+
+
+#include <iostream>
+#include "Unity.h"
+int  main(int argc , char** argv){
+    Hzjp::Unity  uti ;
+    std::cout <<"OK: " << uti.add_numbers(1.0F , 2.0F) << std::endl;
+    return 0 ;
+}
